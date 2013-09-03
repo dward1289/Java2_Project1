35a2c1a71931c5c2000d8ab5a54f5f33b0b5d379
devona <dward1289@fullsail.edu>
2013-09-03 10:34:29 -0400

    Service class created

---
+++ Java2_Project1/src/com/DevonaWard/java2_project1/theService.java
@@ -0,0 +1,20 @@
+package com.DevonaWard.java2_project1;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+public class theService extends IntentService{
+
+	public theService() {
+		super("theService");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
