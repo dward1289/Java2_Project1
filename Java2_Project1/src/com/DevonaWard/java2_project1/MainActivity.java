d3960532a5044fd5f7dab0acedd6e732c0832228
devona <dward1289@fullsail.edu>
2013-09-06 19:01:20 -0400

    Unable to complete web request to pull data or display data. Data is pulled from file and displayed in log.

--- Java2_Project1/src/com/DevonaWard/java2_project1/MainActivity.java
+++ Java2_Project1/src/com/DevonaWard/java2_project1/MainActivity.java
@@ -4,18 +4,22 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Intent intent = new Intent(this, theService.class);
-    	startService(intent);
 	}
 
+	//Onclick for the "Go" button
+	public void startService(View view) {
+		Intent intent = new Intent(this, theService.class);
+		startService(intent);
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- Java2_Project1/src/com/DevonaWard/java2_project1/theService.java
+++ Java2_Project1/src/com/DevonaWard/java2_project1/theService.java
@@ -1,48 +1,81 @@
 package com.DevonaWard.java2_project1;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
 import android.util.Log;
 
 
 
 
+
+
 public class theService extends IntentService{
+	JSONArray teamStat;
+	ArrayList<String> teamRanks = new ArrayList<String>();
+	ArrayList<String> teamName = new ArrayList<String>();
+	ArrayList<String> teamArea = new ArrayList<String>();
+	ArrayList<String> teamDivision = new ArrayList<String>();
 
-	public theService() {
-		super("theService");
-		
-		Log.i("SERVICE", "Intent Service started");
-	}
+  public theService() {
+      super("theService");
+  } 
+     
 
-	@Override
-	protected void onHandleIntent(Intent intent) {
-	
-	}
-	
+  @Override
+  protected void onHandleIntent(Intent intent) {
+	  Log.d("TAG", "Service started");
+	        String JSONString;
+	        try {
 
-	  @Override
-	  public void onCreate() {
-	    
-	  }
+	            //open the inputStream to the file 
+	            InputStream inputStream = getAssets().open("document.json");
 
-	  @Override
-	  public int onStartCommand(Intent intent, int flags, int startId) {
-		
-		  return 0;
-	     
-	  }
+	            int sizeOfJSONFile = inputStream.available();
 
-	  @Override
-	  public IBinder onBind(Intent intent) {
-	      // We don't provide binding, so return null
-	      return null;
-	  }
-	  
-	  @Override
-	  public void onDestroy() {
-	     
-	  }
+	            //array that will store all the data 
+	            byte[] bytes = new byte[sizeOfJSONFile];
 
+	            //reading data into the array from the file
+	            inputStream.read(bytes);
+
+	            //close the input stream
+	            inputStream.close();
+
+	            JSONString = new String(bytes, "UTF-8");
+	            teamStat = new JSONArray(JSONString);
+	            
+	            //Adding pieces of the data to arrays.
+	            for (int i=0; i<teamStat.length(); i++) {
+	              String  theCity = teamStat.getJSONObject(i).getString("first_name");
+	              String  theTeam = teamStat.getJSONObject(i).getString("full_name");
+	              String  theDiv = teamStat.getJSONObject(i).getString("division");
+	              String  theArena = teamStat.getJSONObject(i).getString("site_name");
+	              teamRanks.add(theCity);
+	              teamName.add(theTeam); 
+	          	  teamArea.add(theDiv); 
+	          	  teamDivision.add(theArena); 
+	         }
+	            //All data logged
+	            Log.i("Cities:", teamRanks.toString()); 
+	            Log.i("Teams:", teamName.toString()); 
+	            Log.i("Divisions:", teamDivision.toString()); 
+	            Log.i("Arenas:", teamArea.toString()); 
+	            
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	            return;
+	        }
+	        catch (JSONException x) {
+	            x.printStackTrace();
+	            return;
+	        }
+	   } 
 }
