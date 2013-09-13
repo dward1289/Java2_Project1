905fcc5a1d71f27fbbe787ed2f6d7eda410aff86
devona <dward1289@fullsail.edu>
2013-09-12 21:28:57 -0400

    JSON is saved and can be read but unable to display in UI

--- Java2_Project1/src/com/DevonaWard/java2_project1/theService.java
+++ Java2_Project1/src/com/DevonaWard/java2_project1/theService.java
@@ -1,15 +1,23 @@
 package com.DevonaWard.java2_project1;
 
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
 
 
@@ -17,13 +25,12 @@ import android.util.Log;
 
 
 
-public class theService extends IntentService{
-	JSONArray teamStat;
-	ArrayList<String> teamRanks = new ArrayList<String>();
-	ArrayList<String> teamName = new ArrayList<String>();
-	ArrayList<String> teamArea = new ArrayList<String>();
-	ArrayList<String> teamDivision = new ArrayList<String>();
 
+public class theService extends IntentService{
+	Message message;
+	Messenger messenger;
+	URL actualURL;
+	Context context = this;
   public theService() {
       super("theService");
   } 
@@ -31,51 +38,67 @@ public class theService extends IntentService{
 
   @Override
   protected void onHandleIntent(Intent intent) {
-	  Log.d("TAG", "Service started");
-	        String JSONString;
-	        try {
-
-	            //open the inputStream to the file 
-	            InputStream inputStream = getAssets().open("document.json");
-
-	            int sizeOfJSONFile = inputStream.available();
-
-	            //array that will store all the data 
-	            byte[] bytes = new byte[sizeOfJSONFile];
-
-	            //reading data into the array from the file
-	            inputStream.read(bytes);
-
-	            //close the input stream
-	            inputStream.close();
-
-	            JSONString = new String(bytes, "UTF-8");
-	            teamStat = new JSONArray(JSONString);
-	            
-	            //Adding pieces of the data to arrays.
-	            for (int i=0; i<teamStat.length(); i++) {
-	              String  theCity = teamStat.getJSONObject(i).getString("first_name");
-	              String  theTeam = teamStat.getJSONObject(i).getString("full_name");
-	              String  theDiv = teamStat.getJSONObject(i).getString("division");
-	              String  theArena = teamStat.getJSONObject(i).getString("site_name");
-	              teamRanks.add(theCity);
-	              teamName.add(theTeam); 
-	          	  teamArea.add(theDiv); 
-	          	  teamDivision.add(theArena); 
-	         }
-	            //All data logged
-	            Log.i("Cities:", teamRanks.toString()); 
-	            Log.i("Teams:", teamName.toString()); 
-	            Log.i("Divisions:", teamDivision.toString()); 
-	            Log.i("Arenas:", teamArea.toString()); 
-	            
-	        } catch (IOException ex) {
-	            ex.printStackTrace();
-	            return;
-	        }
-	        catch (JSONException x) {
-	            x.printStackTrace();
-	            return;
-	        }
-	   } 
+	  
+	try {
+		actualURL = new URL ("https://erikberg.com/nba/teams.json");
+	} catch (MalformedURLException e2) {
+		// TODO Auto-generated catch block
+		e2.printStackTrace();
+	}
+	  
+	  ConnectivityManager connectManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+	  NetworkInfo netInfo = connectManager.getActiveNetworkInfo();
+	  
+	  //Check network connection
+	  if(netInfo != null && netInfo.isConnected()){
+		  try{ 
+			  String response = "";
+			  
+			  //Get connected
+			  URLConnection connection = actualURL.openConnection();
+			  BufferedInputStream BIS = new BufferedInputStream(connection.getInputStream());
+			  
+			  byte[] contentBytes = new byte[1024];
+			  int bytesRead = 0;
+			  StringBuffer stringBuffer = new StringBuffer();
+			  
+			  while ((bytesRead = BIS.read(contentBytes)) != -1){
+				  response = new String(contentBytes, 0, bytesRead);
+				  stringBuffer.append(response);
+			  }
+			  response = stringBuffer.toString();
+			  
+			  
+			
+			  
+			  //Verify  JSON string
+			  JSONArray json = null;
+			  try{
+				  json = new JSONArray(response);
+				  Log.i("JSON IS HERE", "The json is valid");
+				  
+				  //Save json data
+				  writeRead.storeObjectFile(context, "teamsJSON", json.toString(), false);
+				  writeRead.storeStringFile(context, "teamsJSONString", json.toString(), false);
+			  }catch (JSONException e){
+				  Log.e("JSON storeJSON", e.getMessage().toString());
+			  }
+			  
+			  String team = "";
+			  for(int i = 0; i<json.length(); i++){
+				  try{
+					  team = json.getJSONObject(i).getString("team_id".toString());
+				  } catch (JSONException e){
+					  e.printStackTrace();
+				  }
+				  Log.d("DATA PARSED",team);
+			  }
+		  }catch(MalformedURLException e){
+			  e.printStackTrace();
+	  } catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+  }
 }
